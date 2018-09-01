2df0ab0035e83447ed3e734e626cf93e4e81ef0b
jeanetteobr <jeanette.obr@gmail.com>
2018-09-01 16:46:00 -0400

    test

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-import 'shoelace-css/dist/shoelace.css'
+mport 'shoelace-css/dist/shoelace.css'
 import './styles.css'
 import Card from './src/Card'
 import Router from 'vanilla-router'
 import Api from './src/Api'
 
-const api = new Api()
-
-document.addEventListener('DOMContentLoaded', function (event) {
-  Card.getAll().then(allTheCards => {
-    for (let card of allTheCards) {
-      createCardDom(card)
-    }
-  })
-
-  const router = new Router({
-    mode: 'history'
-  })
-
-  router.add('cards/{id}', (id) => {
-    api.get()
-      .then(res => {
-        const cardData = res.body
-        const card = new Card(cardData)
-        const dom = createCardDom(card)
-        document.getElementById('cards-container').innerHTML = ''
-        document.getElementById('cards-container').appendChild(dom)
-      })
-  })
-
-  router.addUriListener()
-  router.check()
-
-  document.getElementById('submit-button').addEventListener('click', function (event) {
-    event.preventDefault()
-    let cardData = {
-      answer: document.getElementById('input-answer').value,
-      question: document.getElementById('input-question').value
-    }
-    let newCard = new Card(cardData)
-    newCard.create().then(card => {
-      createCardDom(card)
-    })
-  })
+const router = new Router({
+  mode: 'history'
 })
 
+router.add('', (id) => {
+  document.getElementById('cards-container').innerHTML = ''
+  const h1 = document.createElement('h1')
+  h1.innerText = 'Main page'
+  document.getElementById('cards-container').appendChild(h1)
+})
+
+router.add('cards/{id}', (id) => {
+  const api = new Api()
+  api.get(id)
+    .then(res => {
+      const cardData = res.body
+      const card = new Card(cardData)
+      const dom = createCardDom(card)
+      document.getElementById('cards-container').innerHTML = ''
+      document.getElementById('cards-container').appendChild(dom)
+    })
+})
+
+router.addUriListener()
+router.check()
+
+
+  // document.getElementById('submit-button').addEventListener('click', function (event) {
+  //   event.preventDefault()
+  //   let cardData = {
+  //     answer: document.getElementById('input-answer').value,
+  //     question: document.getElementById('input-question').value
+  //   }
+  //   let newCard = new Card(cardData)
+  //   newCard.create().then(card => {
+  //     createCardDom(card)
+  //   })
+  // })
+
+
 function createCardDom (card) {
-  let cardsContainer = document.getElementById('cards-container')
   let cardDom = document.createElement('li')
   cardDom.classList.add('flash-card')
   cardDom.dataset.cardId = card.id
@@ -59,8 +58,7 @@ function createCardDom (card) {
   let newEditButton = createEditButton(card)
   cardDom.appendChild(newDeleteButton)
   cardDom.appendChild(newEditButton)
-
-  cardsContainer.prepend(cardDom)
+  return cardDom
 }
 
 function createDeleteButton (card, cardDom) {
